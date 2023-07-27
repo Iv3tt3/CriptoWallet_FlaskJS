9a23535ba655c06fe674af0a80149122d2c8cd4d
Ivette Farre Prat <ivettefarreprat@Ivettes-MacBook-Pro.local>
2023-07-27 21:02:52 +0100

    Improvements and 2 version of JS

--- cripto_wallet/models.py
+++ cripto_wallet/models.py
@@ -3,6 +3,9 @@ import requests
 from cripto_wallet import app
 from datetime import datetime
 
+available_coins = []
+for value, coin in app.config.get("COIN_OPTIONS_LIST"):
+    available_coins.append(coin)
 
 class Calculator:
     def __init__(self):
@@ -18,24 +21,25 @@ class Calculator:
         url = f"https://rest.coinapi.io/v1/exchangerate/{From_Coin}/{To_Coin}?apikey={apikey}"
         try: 
             consult_response = requests.get(url)
-            data = consult_response.json() 
+            data = consult_response.json()
             if consult_response.status_code == 200:
                 Amount_To = float(Amount_From) * float(data['rate'])
                 data = self.data_to_dict(From_Coin, To_Coin, Amount_From, Amount_To, data['rate'])
-                self.rate = (data['rate'])
-                self.From_Coin = From_Coin
-                self.To_Coin = To_Coin
-                self.time = datetime.utcnow().isoformat()
-                self.Amount_From = Amount_From
-                self.Amount_To = Amount_To
                 return True, data
             else:
                 return False, str(consult_response.status_code) + data['error']    
-        
         except requests.exceptions.RequestException as error_str:
             return False, str(error_str) + str(url)
     
-    def reset(self):
+    def save_data(self, data):
+        self.rate = (data['rate'])
+        self.From_Coin = data["From_Coin"]
+        self.To_Coin = data["To_Coin"]
+        self.time = datetime.utcnow().isoformat()
+        self.Amount_From = data["Amount_From"]
+        self.Amount_To = data["Amount_To"]
+    
+    def reset_data(self):
         self.From_Coin = self.To_Coin = self.rate = self.Amount_From = self.Amount_To = self.time = ""
 
     def data_to_dict(self, From_Coin, To_Coin, Amount_From, Amount_To, rate):
@@ -48,6 +52,14 @@ class Calculator:
         }
 
     def validate_data(self, Amount_From, From_Coin, To_Coin, Amount_To):
+        amount = dao.wallet_balance[From_Coin][1] - dao.wallet_balance[From_Coin][0]
+        if From_Coin != "EUR" and amount == 0:
+            error = f"No balance {From_Coin} in your wallet\nPlease check your status and select a From Coin available in your wallet"
+            return False, error
+        if From_Coin != "EUR" and amount < float(Amount_From):
+            error = f"Not enought balance of {From_Coin}in your wallet\nYour currently balance is {amount}{From_Coin}"
+            return False, error   
+        
         time_now = datetime.utcnow().isoformat()
         dif_time = datetime.fromisoformat(time_now)-datetime.fromisoformat(self.time)
         if int(dif_time.seconds) // 60 > 5:
@@ -63,8 +75,9 @@ class Calculator:
 class DAOSqlite:
     def __init__(self, data_path):
         self.path = data_path
+        self.wallet_balance = {}
+        self.main_error = ""
         self.create_table_init()
-        self.wallet_transactions = []
 
     #Create a db sqlite table if not exist in the path (check path in FLASK_PATH_SQLITE in .env)
     
@@ -90,7 +103,7 @@ class DAOSqlite:
 
     def get_all_transactions(self):
         query = """
-        SELECT Date, Time, From_Coin, Amount_From, To_Coin, Amount_To
+        SELECT id, Date, Time, From_Coin, Amount_From, To_Coin, Amount_To
         FROM transactions
         ORDER by date
         ;"""
@@ -99,30 +112,61 @@ class DAOSqlite:
         cur.execute(query)
         data = cur.fetchall()
         transaction_list = []
+        self.wallet_balance = {}
+        for coin in available_coins:
+            self.wallet_balance[coin] = [0,0]
         for transaction in data:
             transaction = self.convert_to_dict(transaction)
+            self.validate_data(transaction)
+            transaction = self.add_in_wallet_balance(transaction)
             transaction_list.append(transaction)
         conn.close()
-        self.wallet_transactions = transaction_list
         return transaction_list
-    
-    # Convert a transaction into a dict. Needed for get_all_transactions method
-    #FALTA VALIDAR TRANSACCION
 
     def convert_to_dict(self, transaction):
         return {
-            "Date" : transaction[0],
-            "Time" : transaction[1],
-            "From_Coin"	: transaction[2],
-            "Amount_From" : transaction[3],
-            "To_Coin" : transaction[4],
-            "Amount_To"	: transaction[5]
+            "id": transaction[0],
+            "Date" : transaction[1],
+            "Time" : transaction[2],
+            "From_Coin"	: transaction[3],
+            "Amount_From" : transaction[4],
+            "To_Coin" : transaction[5],
+            "Amount_To"	: transaction[6]
         }
     
-    def insert_transaction(self, calculs):
+    def validate_data(self, transaction):
+        
+        try:
+            datetime.strptime(transaction['Date'],'%Y-%m-%d')
+            datetime.strptime(transaction['Time'],'%H:%M:%S')
+        except:
+            raise ValueError(f"Error in data validation\nerror details: format error in data/time of transaction id {transaction['id']}")
+        
+        if transaction['From_Coin'] not in available_coins or transaction['To_Coin'] not in available_coins:
+            raise ValueError(f"Error in data validation\nerror details: format error in from/to coin of transaction id {transaction['id']}")
+        
+        try:
+            Amount_From = float(transaction['Amount_From'])
+            Amount_To = float(transaction['Amount_To'])
+            if Amount_From <= 0 or Amount_To <= 0:
+                raise ValueError(f"Error in data validation\nerror details: format error in from/to amount of transaction id {transaction['id']}")
+        except:
+            raise ValueError(f"Error in data validation\nerror details: format error in from/to amount of transaction id {transaction['id']}")
+        
+        return transaction
+    
+    def add_in_wallet_balance(self, transaction):
+        self.wallet_balance[transaction["From_Coin"]][0] += transaction["Amount_From"]
+        self.wallet_balance[transaction["To_Coin"]][1] += transaction["Amount_To"]
+
+        return transaction
+    
+    def insert_transaction(self, calculator):
         time_now = datetime.utcnow().isoformat()
         date = time_now[0:10]
         time = time_now[11:19]
+        transaction = self.convert_to_dict(('no_id', date, time, calculator.From_Coin, calculator.Amount_From, calculator.To_Coin, calculator.Amount_To))
+        self.validate_data(transaction)
         query = """
         INSERT INTO transactions
                 (Date, Time, From_Coin, Amount_From, To_Coin, Amount_To)
@@ -131,55 +175,49 @@ class DAOSqlite:
         conn = sqlite3.connect(self.path)
         cur = conn.cursor()
         
-        cur.execute(query, (date, time, calculs.From_Coin, calculs.Amount_From, calculs.To_Coin, calculs.Amount_To))
+        cur.execute(query, (transaction["Date"], 
+                            transaction["Time"], 
+                            transaction["From_Coin"], 
+                            transaction["Amount_From"], 
+                            transaction["To_Coin"], 
+                            transaction["Amount_To"]))
         conn.commit()
+    
+        calculator.reset_data()
+
 
     def wallet_status(self):
-        try:
-            wallet_criptos = []
-            wallet_value = 0
-            invested_euros = 0
-            refund_euros = 0
-            coin_options = app.config.get("COIN_OPTIONS_LIST")
-            for coin_text, coin in coin_options:
-
-                cripto_amount=0
-                cripto_value=0
-
-                for transaction in self.wallet_transactions:
-
-                    if coin == "EUR":
-                        if transaction["From_Coin"] == coin:
-                            invested_euros -= transaction["Amount_From"]
-                        if transaction["To_Coin"] == coin:
-                            refund_euros += transaction["Amount_To"]
-                
-                    else:
-                        if transaction["From_Coin"] == coin:
-                            cripto_amount -= transaction["Amount_From"]
-                        if transaction["To_Coin"] == coin:
-                            cripto_amount += transaction["Amount_To"]
-                
+        wallet_criptos = []
+        invested_euros = 0
+        refund_euros = 0
+        wallet_value = 0
+        for coin in self.wallet_balance:
+            cripto_amount = 0
+            cripto_value = 0
+            if coin == "EUR":
+                invested_euros = self.wallet_balance[coin][0]
+                refund_euros = self.wallet_balance[coin][1]
+            else:
+                cripto_amount = self.wallet_balance[coin][1] - self.wallet_balance[coin][0]
                 if cripto_amount !=0:
-                    calculator.get_rate(coin,"EUR",cripto_amount)
-                    cripto_value = calculator.rate * cripto_amount
-                    wallet_value += cripto_value
-                    wallet_criptos.append((coin, cripto_amount, cripto_value)) 
-            
-            investment_result = refund_euros + invested_euros
+                    status, data = calculator.get_rate(coin,"EUR",cripto_amount)
+                    if status:
+                        cripto_value = data["Amount_To"]
+                        wallet_value += cripto_value
+                        wallet_criptos.append((coin, cripto_amount, cripto_value))
+                    else:
+                        return False, data
+        
+        investment_result = refund_euros - invested_euros
 
-            data = {
+        data = {
                 "wallet_criptos" : wallet_criptos, 
                 "wallet_value": wallet_value,
                 "invested_euros": invested_euros,
                 "refund_euros": refund_euros, 
                 "investment_result": investment_result
             }
-            
-            return True, data
-
-        except:
-            data = "ERROR. Your wallet status is not available. Please, contact support"
-            return False, data
+        return data
 
 calculator = Calculator()
+dao = DAOSqlite(app.config.get("PATH_SQLITE"))
--- cripto_wallet/routes.py
+++ cripto_wallet/routes.py
@@ -1,31 +1,33 @@
 from cripto_wallet import app
 from flask import render_template, request, redirect, url_for
-from cripto_wallet.models import DAOSqlite, calculator
+from cripto_wallet.models import dao, calculator
 import sqlite3
 
-dao = DAOSqlite(app.config.get("PATH_SQLITE"))
 
 @app.route("/")
 def index():
-    try:
-        return render_template("index.html")
-    except:
+    js_path = (app.config.get("JAVASCRIPT_PATH"))
+    if js_path == "/static/js/app_v1.js" or js_path == "/static/js/app_v2.js": 
+        return render_template("index.html", js_path = js_path)
+    else:
+        dao.main_error = "Error info: JS path is not well format in the env file"
         return redirect("/fatalerror")
     
 @app.route("/fatalerror")
 def fatal_error():
-    return render_template("fatalerror.html")
+    return render_template("fatalerror.html", error = dao.main_error)
 
 @app.route("/api/v1/transactions")
 def all_transactions():
     try:
         transactions = dao.get_all_transactions()
-
         coin_options = app.config.get("COIN_OPTIONS_LIST")
+        wallet_balance = dao.wallet_balance
         response = {
             "ok": True,
             "data": transactions,
-            "coin_options": coin_options
+            "coin_options": coin_options,
+            "wallet_balance": wallet_balance
         }
         return response
     except sqlite3.Error as e:
@@ -37,24 +39,32 @@ def all_transactions():
     except ValueError as e:
         response = {
             "ok": False,
-            "data": "Please, contact support\nerror info:" + str(e)
+            "data": "Please, contact support\nerror info: " + str(e)
         }
         return response, 400
 
 @app.route("/api/v1/rate/<From_Coin>/<To_Coin>/<Amount_From>")
 def get_rate(From_Coin, To_Coin, Amount_From):
-    status, data = calculator.get_rate(From_Coin, To_Coin, Amount_From)
-    if status:
+    try:
+        status, data = calculator.get_rate(From_Coin, To_Coin, Amount_From)
+        if status:
+            calculator.save_data(data)
+            response = {
+                "ok": True,
+                "data": data
+            }
+            return response
+        else:
+            response = {
+                "ok": False,
+                "data": "Calculator is not avaliable at this moment.\nPlease contact support.\n\nError in CoinAPI.io request.\nMore info: " + data
+            }
+            return response, 400
+    except Exception as e:
         response = {
-            "ok": True,
-            "data": data
-        }
-        return response
-    else:
-        response = {
-            "ok": False,
-            "data": "Calculator is not avaliable at this moment.\nPlease contact support.\n\nError in CoinAPI.io request.\nMore info: " + data
-        }
+                "ok": False,
+                "data": "Please, contact support\nError Exception type in get rate route\n" + str(e)
+            }
         return response, 400
 
 @app.route("/api/v1/insert", methods=["POST"])
@@ -67,49 +77,60 @@ def insert_transaction():
             request.json.get("Amount_To"),
             )
         if data_validation:
-            try:
-                dao.insert_transaction(calculator)
-                response = {
-                    "ok": True,
-                    "data": "Purchase order completed"
-                }
-                return response, 201
-            
-            except sqlite3.Error as e:
-                response = {
-                    "ok": False,
-                    "data": "Unexpected error: DB is currently not available.\nPlease contact support\n\nMore info: " + e
-                }
-                return response, 400
-
+            dao.insert_transaction(calculator)
+            response = {
+                "ok": True,
+                "data": "SUCCESSFUL!\n\nYour purchase order has been completed successfully.\nPlease refresh website page"
+            }
+            return response, 201
         else:
             response = {
                     "ok": False,
                     "data": error_info
                 }
             return response, 400
-
+        
+    except sqlite3.Error as e:
+        response = {
+            "ok": False,
+            "data": "Your purchase has NOT been completed.\nPlease contact support\n\nMore info: DB is currently not available.\n" + str(e)
+        }
+        return response, 400
+    
     except ValueError as e:
         response = {
                     "ok": False,
-                    "data": "Your purchase has NOT been completed.\nPlease contact support\n\nMore info: " + e
+                    "data": "Your purchase has NOT been completed.\nPlease contact support\n\nMore info: " + str(e)
                 }
+        return response, 400
+    
+    except Exception as e:
+        response = {
+                "ok": False,
+                "data": "Your purchase has NOT been completed.\nPlease contact support\n\nMore info: Exception type in insert trasaction route\n" + str(e)
+            }
         return response, 400
     
 @app.route("/api/v1/status")
 def investment_status():
-    status, data = dao.wallet_status()
-
-    if status:
+    try:
+        data = dao.wallet_status()
         response = {
             "ok": True, 
             "data": data
         }
         return response
-    
-    else:
+        
+    except ValueError as e:
         response = {
             "ok": False, 
-            "data": data
+            "data": "Your wallet status is not available. Please, contact support\nMore info: ValueError in status route"
         }
-        return response
+        return response, 400
+        
+    except Exception as e:
+        response = {
+                "ok": False,
+                "data": "Your wallet status is not available. Please, contact support\nMore info: Exception type in status route\n" + str(e)
+            }
+        return response, 400
